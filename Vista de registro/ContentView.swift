36f340f9225aad994dd3e56a2e64c332b3cc6fa9
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2021-02-14 13:42:19 -0300

    Initial Commit

---
+++ Vista de registro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vista de registro
+//
+//  Created by Jacob Aguilar on 14-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vista de registro/Vista_de_registroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vista_de_registroApp.swift
+//  Vista de registro
+//
+//  Created by Jacob Aguilar on 14-02-21.
+//
+
+import SwiftUI
+
+@main
+struct Vista_de_registroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
